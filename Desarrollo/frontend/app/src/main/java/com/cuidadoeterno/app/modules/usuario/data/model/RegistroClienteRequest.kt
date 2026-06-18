57637a9107adc58b7be32c4149fe6645fd7e6548
Ccarlito <carl.munozu@duocuc.cl>
2026-06-18 02:41:01 -0400

    feature/frontend: Se guarda el desarrollo del frontend antes de un pull.

--- Desarrollo/frontend/app/src/main/java/com/cuidadoeterno/app/modules/usuario/data/model/RegistroClienteRequest.kt
+++ Desarrollo/frontend/app/src/main/java/com/cuidadoeterno/app/modules/usuario/data/model/RegistroClienteRequest.kt
@@ -1,4 +1,19 @@
 package com.cuidadoeterno.app.modules.usuario.data.model
 
+import java.time.LocalDate
 
+data class RegistroClienteRequest (
+    val rut: String,
+    val nombre: String,
+    val apPaterno: String,
+    val apMaterno: String,
+    val email: String,
+    val telefono: String,
+    val fechaNacimiento: LocalDate,
+    val genero: String,
+    val nombreUsuario: String,
+    val clave: String,
+    val prefNotificacion: String
+
+)
 
--- Desarrollo/frontend/app/src/main/java/com/cuidadoeterno/app/modules/usuario/data/model/RegistroCuidadorRequest.kt
+++ Desarrollo/frontend/app/src/main/java/com/cuidadoeterno/app/modules/usuario/data/model/RegistroCuidadorRequest.kt
@@ -1,2 +1,16 @@
 package com.cuidadoeterno.app.modules.usuario.data.model
 
+import java.time.LocalDate
+
+data class RegistroCuidadorRequest (
+    val rut: String,
+    val nombre: String,
+    val apPaterno: String,
+    val apMaterno: String,
+    val email: String,
+    val telefono: String,
+    val fechaNacimiento: LocalDate,
+    val genero: String,
+    val nombreUsuario: String,
+    val clave: String
+)
