2c56746a17bf5e0d990e84ea5310351b54056ce5
ImChar1 <soucharuno@gmail.com>
2026-06-24 10:47:27 -0400

    develop: Se realizan ajustes de pruebas de integracion.

--- Desarrollo/infra/etapa_2/main.tf
+++ Desarrollo/infra/etapa_2/main.tf
@@ -35,6 +35,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
   tags = { Name = "${var.project_name}-subnet-public" }
 }
@@ -42,6 +43,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = false
   tags = { Name = "${var.project_name}-subnet-private" }
 }
